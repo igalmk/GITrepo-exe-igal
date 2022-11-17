92d119c5a91d40140ab397086e103a4839b86b20
igalmk <igal678@gmail.com>
2022-11-17 14:59:15 +0200

    igal.java was changed

--- Igal.java
+++ Igal.java
@@ -2,6 +2,7 @@ public class Igal{
     public static void main(String[] args){
         for(int i = 1; i<=10; i++){
             System.out.println("Igal Maikis: "+ i);
+            System.out.println("I also was changed");
         }
     }
 }
